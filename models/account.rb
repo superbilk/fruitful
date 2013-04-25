9811926785aa4d583515334b050d1b8539d1d2d8
Chris Schmidt <chris@superbilk.org>
2013-04-25 10:54:39 +0200

    add adminurl to account model

--- models/account.rb
+++ models/account.rb
@@ -1,9 +1,10 @@
 class Account
   include DataMapper::Resource
 
-  property    :id,   Serial
-  property    :url,  String, :unique => true
-  property    :name, String
+  property    :id,        Serial
+  property    :url,       String, :unique => true
+  property    :name,      String
+  property    :adminurl,  String, :unique => true
   timestamps  :at
 
   has n, :votes
